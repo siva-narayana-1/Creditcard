ca957d546602698ec818b949f6e3acad67201cc6
siva-narayana-1 <sivanarayanamadhala@gmail.com>
2025-09-18 19:58:57 +0530

    done

--- loggers.py
+++ loggers.py
@@ -12,22 +12,6 @@ class Logger:
             formate = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
             handler.setFormatter(formate)
-=======
-import logging
-import sys
-from cgitb import handler
-
-class Logs:
-    def get_logger(script_name):
-        try:
-            logger = logging.getLogger(script_name)
-            logger.setLevel(logging.DEBUG)
-
-            # Create a file handler for the script
-            handler = logging.FileHandler(f'C:\\credict_card\\logs\\{script_name}.log')
-            formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-            handler.setFormatter(formatter)
->>>>>>> b1bbf0c870a1d8ca10d3460eb94dab38a20b0b6a
             logger.addHandler(handler)
 
             return logger
